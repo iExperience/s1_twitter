6af4cf250efaf332c71870bdd47e1b10ade6f453
Salman Ansari <salman.ansari@gmail.com>
2014-06-17 10:46:53 +0200

    enable tweets and users spec

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -5,19 +5,27 @@ describe "Tweets" do
 
   it "should be able to add new tweets" do
     # visit tweets list page
+    visit "/tweets"
 
     # click on the New Tweet link
+    click_link "New Tweet"
 
     # select @daretorant as the user
+    select user.handle, :from => "User"
 
     # fill in the Body with "A testing tweet!"
+    fill_in "Body", :with => "A testing tweet!"
 
     # click the button "Create Tweet"
+    click_button "Create Tweet"
 
     # the page should show "Tweet was successfully created."
+    page.should have_content("Tweet was successfully created.")
 
     # click the "Back" link
+    click_link "Back"
 
     # the page should show "A testing tweet!"
+    page.should have_content("A testing tweet!")
   end  
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 describe "Users" do
   it "should be able to add new users" do
     # visit the Users list page
+    visit "/users"
 
     # click the "New User" link
+    click_link "New User"
 
     # fill in the Handle with "@daretorant"
+    fill_in "Handle", :with => "@fizz_can"
 
     # click the "Create User" button
+    click_button "Create User"
 
     # the page should show "User was successfully created."
+    page.should have_content("User was successfully created.")
   end
 end
